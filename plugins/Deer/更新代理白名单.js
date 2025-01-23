cf9a160fb5aeb579460375ac21f177d44af2f30c
鹿 <70961031+deerwan@users.noreply.github.com>
2025-01-23 11:19:59 +0800

    Update 更新代理白名单.js

--- plugins/Deer/更新代理白名单.js
+++ plugins/Deer/更新代理白名单.js
@@ -1,236 +1,222 @@
 /**
- * @author 小九九 t.me/gdot0
- * @name 更新代理白名单
+ * @author 小九九
+ * @name 更换rabbitpro反代
  * @origin 小九九
- * @version 1.1.0
- * @rule ^chwlst$
- * @description 更换jl、su、51、yyy、ipzan白名单
+ * @team 小九九
+ * @version 1.0
+ * @rule ^crbfd$
+ * @description 检查反代列表，更换可用的rabbitpro反代
  * @admin true
  * @public false
- * @cron 0 0 *\/1 * * *
  * @priority 1000
  */
 
-//配置
-const yyytoken = ""; //优易云token
+const jsonSchema = BncrCreateSchema.object({
+    rabbit: BncrCreateSchema.object({
+        base: BncrCreateSchema.string().setTitle("地址").setDescription(`最后不要带/`).setDefault("http://172.0.0.1:1234"),
+        name: BncrCreateSchema.string().setTitle("用户名").setDescription(`登录后台用的那个用户名`).setDefault("admin"),
+        passwd: BncrCreateSchema.string().setTitle("密码").setDescription(`登录后台用的那个密码`).setDefault("password"),
+    }).setTitle("兔子面板"),
+    rlist: BncrCreateSchema.array(BncrCreateSchema.string().setTitle("地址").setDescription(`前面不要http(s)://，最后不要带/`).setDefault("mr-orgin.1888866.xyz"))
+        .setTitle("反代地址")
+        .setDescription(`点击右下角+增加更多地址`)
+        .setDefault([
+            "rabbit.cfyes.tech",
+            "mr-orgin.1888866.xyz",
+            "jd-orgin.1888866.xyz",
+            "mr.118918.xyz",
+            "host.257999.xyz",
+            "log.madrabbit.eu.org",
+            "fd.gp.mba:6379",
+        ]),
+});
+const ConfigDB = new BncrPluginConfig(jsonSchema);
 
-const d51Key = ""; //51代理key
-
-const suKey = ""; //速代理key
-
-const jltradeNo = ""; //巨量订单号
-const jlkey = ""; //巨量key
-
-const ipzanNo = ""; //品赞订单号
-const ipzanUserId = ""; //品赞用户id
-const 品赞登录密码 = "";
-const 品赞套餐提取密匙 = "";
-const 品赞签名密匙 = "";
-
-const request = require("util").promisify(require("request"));
-const got = require("got");
-const DB = new BncrDB("djunDB"); //从ipChange.js中调取本地ip
-
-const yyyurl = "http://data.yyyip.cn:8888/whiteip_api?";
-//const url = "https://4.ipw.cn/";
+const db = new BncrDB("rabbit");
 
 module.exports = async (s) => {
-    /*
-    let data = await request({
-        url: url,
-        method: 'get'
-    });
-    let newip = data.body;
-    console.log(newip);
-    if (newip.split('.').length != 4) {
-        console.log("无效ip");
-        return;
+    await ConfigDB.get();
+    if (!Object.keys(ConfigDB.userConfig).length) {
+        return await s.reply('请先发送"修改无界配置",或者前往前端web"插件配置"来完成插件首次配置');
     }
-    */
-    let newip = await DB.get("local_ip");
-    console.log(newip);
-    if (newip) {
-        let adminNotice = "";
-        Promise.all([
-            //并发提交更换白名单请求，如果有新请求，请加到这个数列里面
-            jl(),
-            yyy(),
-            su(),
-            ipzan(),
-        ]).then(() => {
-            //console.log("更换白名单结果：\n" + adminNotice);
-            sysMethod.pushAdmin({
-                platform: ["tgBot"],
-                type: "text",
-                msg: "更换白名单结果：\n" + adminNotice.slice(0, -2),
-            });
-        });
+    const baseUrl = ConfigDB.userConfig.rabbit.base;
+    const name = ConfigDB.userConfig.rabbit.name;
+    const passwd = ConfigDB.userConfig.rabbit.passwd;
+    const urlArr = ConfigDB.userConfig.rlist;
+    if (!global.crbfd_lock) {
+        global.crbfd_lock = true;
+        try {
+            const axios = require("axios");
+            const regBaseUrl = regHttpUrl(baseUrl);
+            const authToken = await auth(name, passwd);
+            if (!authToken) throw new Error("兔子后台鉴权失败");
+            let config = await getConfig(authToken);
+            let oldAuthUrl = config.ServerHost;
+            let n = urlArr.indexOf(oldAuthUrl) ?? 0;
+            if (!config) throw new Error("network problem");
+            const availUrl = await testAvailUrl(authToken, urlArr, n);
+            if (!availUrl) throw new Error("no url available");
+            await db.set("auth_url", `http://${availUrl}`);
+            config.ServerHost = availUrl;
+            await saveConfig(authToken, config);
 
-        //await updateIp();
+            await sysMethod.sleep(5);
+            global.crbfd_lock = false;
 
-        //各家代理更换白名单的方法，可自行补充
-        async function jl() {
-            //巨量
-            //替换白名单
-            const jlApi = "http://v2.api.juliangip.com/dynamic/getwhiteip?trade_no=" + jltradeNo + "&sign=" + jlwlSign();
-            let jlWhiteList = "";
-            try {
-                jlWhiteList = await got(jlApi).json();
-            } catch (err) {
-                console.log("巨量获取白名单失败");
-                adminNotice += "巨量get error\n";
-                return;
-            }
-            //console.log(jlWhiteList);
-            try {
-                const jlWhiteIp = jlWhiteList.data.current_white_ip;
-                function jlrpSign() {
-                    const string = "new_ip=" + newip + "&old_ip=" + jlWhiteIp + "&trade_no=" + jltradeNo + "&key=" + jlkey;
-                    const md5 = require("md5");
-                    const sign = md5(string);
-                    return sign;
+            async function auth(name, passwd) {
+                await axios({
+                    method: "post",
+                    url: regBaseUrl + "/admin/auth",
+                    data: { username: name, password: passwd },
+                })
+                    .then((response) => {
+                        result = response.data;
+                    })
+                    .catch((error) => {
+                        console.log(error);
+                        sysMethod.pushAdmin({
+                            platform: [],
+                            msg: "连接rabbitpro后台失败",
+                        });
+                    });
+                if (!result) {
+                    return;
+                } else if (result.code == 401) {
+                    sysMethod.pushAdmin({
+                        platform: [],
+                        msg: "更换rabbitpro反代auth：" + result.msg,
+                    });
+                    return;
                 }
-                let jlWhiteReplace = await request({
-                    url:
-                        "http://v2.api.juliangip.com/dynamic/replaceWhiteIp?trade_no=" +
-                        jltradeNo +
-                        "&new_ip=" +
-                        newip +
-                        "&old_ip=" +
-                        jlWhiteIp +
-                        "&sign=" +
-                        jlrpSign(),
+                return result.access_token;
+            }
+
+            async function getConfig(authToken) {
+                await axios({
                     method: "get",
-                });
-                //onsole.log(jlWhiteReplace.body);
-                //adminNotice += jlWhiteReplace.body;
-            } catch (err) {
-                console.log("巨量替换白名单失败");
-                adminNotice += "巨量push error\n";
+                    url: regBaseUrl + "/admin/GetConfig",
+                    headers: {
+                        authorization: "Bearer " + authToken,
+                    },
+                })
+                    .then((response) => {
+                        result = response.data;
+                    })
+                    .catch();
+                if (!result) {
+                    return;
+                }
+                return result;
+            }
+
+            async function saveConfig(authToken, config) {
+                await axios({
+                    method: "post",
+                    url: regBaseUrl + "/admin/SaveConfig",
+                    headers: {
+                        authorization: "Bearer " + authToken,
+                    },
+                    data: config,
+                })
+                    .then((response) => {
+                        result = response.data;
+                    })
+                    .catch();
+                if (result?.code == 0) {
+                    sysMethod.pushAdmin({
+                        platform: [],
+                        msg: "更换rabbitpro反代：" + result.msg + "\n现在使用的是：" + config.ServerHost,
+                    });
+                    return;
+                } else {
+                    sysMethod.pushAdmin({
+                        platform: [],
+                        msg: "更换rabbitpro反代：保存设置失败",
+                    });
+                }
                 return;
             }
-            console.log("巨量替换ip成功");
-            adminNotice += "巨量success\n";
-        }
 
-        async function yyy() {
-            //优亦云
-            //删除旧白名单
-            console.log("开始更换");
-            const yyyApi = yyyurl + "method=list&token=" + yyytoken;
-            let yyyWhiteList = await got(yyyApi).json();
-            //console.log(yyyWhiteList);
-            for (const yyyWhiteRec of yyyWhiteList.data) {
-                let yyyWhiteIp = yyyWhiteRec.ip;
-                let yyyWhiteDel = await request({
-                    url: yyyurl + "method=del&token=" + yyytoken + "&ip=" + yyyWhiteIp,
-                    method: "get",
+            async function testAvailUrl(authToken, urlArr, n) {
+                //urlArr = shuffleArray(urlArr);
+                //for (let url of urlArr) {
+                for (let i = 1; i < urlArr.length; i++) {
+                    url = urlArr[(n + i) % urlArr.length];
+                    const regUrl = regHttpUrl(url);
+                    console.log("正在测试：" + regUrl);
+
+                    let result = false;
+                    try {
+                        await axios({
+                            method: "get",
+                            timeout: 5000,
+                            url: regUrl + "/enc/M",
+                        })
+                            .then((response) => {
+                                result = response.data;
+                            })
+                            .catch((response) => {
+                                //if (!response?.response) console.log(response);
+                                result = response.response.data;
+                            });
+                    } catch (e) {
+                        console.log("crbfd testAvail:", e);
+                    }
+                    //if (result.toString().includes("message")) return url;
+                    console.log(result);
+                    if (result && result?.message?.data == "no data") return url;
+
+                    /* await axios({
+                        method: "post",
+                        url: regBaseUrl + "/admin/TestServerHost",
+                        headers: {
+                            authorization: "Bearer " + authToken,
+                        },
+                        data: {
+                            ServerHost: regUrl,
+                        },
+                    })
+                        .then((response) => {
+                            result = response.data;
+                        })
+                        .catch();
+                    if (result.success == true) {
+                        return url;
+                    } */
+                }
+                sysMethod.pushAdmin({
+                    platform: [],
+                    msg: "更换rabbitpro反代：全都不能用",
                 });
-                //console.log(yyyWhiteDel.body);
+                function shuffleArray(array) {
+                    for (let i = array.length - 1; i > 0; i--) {
+                        const j = Math.floor(Math.random() * (i + 1));
+                        [array[i], array[j]] = [array[j], array[i]]; // 使用ES6的解构赋值进行交换
+                    }
+                    return array;
+                }
             }
-            console.log("优亦云删除旧ip成功");
-            //添加新白名单
-            let yyydata = await request({
-                url: yyyurl + "method=add&token=" + yyytoken + "&ip=" + newip,
-                method: "get",
-            });
-            console.log("优亦云添加新ip成功");
-            adminNotice += "优亦云success\n";
-            //console.log(yyydata.body);
-        }
 
-        /*
-        await function d51(){
-            //51
-            //删除旧白名单
-            const d51Api = "http://aapi.51daili.com/whiteIP?op=list&appkey=" + d51Key;
-            let d51WhiteList = await got(d51Api).json();
-            //console.log(d51WhiteList);
-            for (const d51WhiteIp of d51WhiteList.data.list) {
-                let d51WhiteDel = await request({
-                    url: "http://aapi.51daili.com/whiteIP?op=del&appkey=" + d51Key + "&whiteip=" + d51WhiteIp,
-                    method: 'get'
-                });
-                //console.log(d51WhiteDel.body);
+            function regHttpUrl(url) {
+                const httpReg = /^https?:\/\//;
+                if (!httpReg.test(url)) {
+                    url = "http://" + url;
+                }
+                if (baseUrl.endsWith("/")) {
+                    url = url.slice(0, -1);
+                }
+                return url;
             }
-            console.log("51删除旧ip成功");
-            //添加新白名单
-            let d51data = await request({
-                url: "http://aapi.51daili.com/whiteIP?op=add&appkey=" + d51Key + "&whiteip=" + newip,
-                method: 'get'
+        } catch (err) {
+            sysMethod.pushAdmin({
+                platform: [],
+                msg: "更换rabbitpro反代err：" + err,
             });
-            console.log("51添加新ip成功");
-            //console.log(d51data.body);
+            await sysMethod.sleep(5);
+            global.crbfd_lock = false;
+            throw err;
         }
-        */
-
-        async function su() {
-            //速
-            //删除旧白名单
-            const suApi = "https://sudaili.com/whiteIP?op=list&appkey=" + suKey;
-            let suWhiteList = await got(suApi).json();
-            //console.log(d51WhiteList);
-            for (const suWhiteIp of suWhiteList.data.list) {
-                let suWhiteDel = await request({
-                    url: "https://sudaili.com/whiteIP?op=del&appkey=" + suKey + "&whiteip=" + suWhiteIp,
-                    method: "get",
-                });
-                //console.log(d51WhiteDel.body);
-            }
-            console.log("速删除旧ip成功");
-            //添加新白名单
-            let sudata = await request({
-                url: "https://sudaili.com/whiteIP?op=add&appkey=" + suKey + "&whiteip=" + newip,
-                method: "get",
-            });
-            console.log("速添加新ip成功");
-            adminNotice += "速success\n";
-            //console.log(sudata.body);
-        }
-
-        async function ipzan() {
-            //品赞
-            //删除旧白名单
-            const ipzanApi = "https://service.ipzan.com/whiteList-get?no=" + ipzanNo + "&userId=" + ipzanUserId;
-            let ipzanWhiteList = await got(ipzanApi).json();
-            //console.log(ipzanWhiteList);
-            for (const ipzanWhiteRec of ipzanWhiteList.data) {
-                let ipzanWhiteIp = ipzanWhiteRec.id;
-                let ipzanWhiteDel = await request({
-                    url: "https://service.ipzan.com/whiteList-del?no=" + ipzanNo + "&userId=" + ipzanUserId + "&ip=" + ipzanWhiteIp,
-                    method: "get",
-                });
-                //console.log(ipzanWhiteDel.body);
-            }
-            console.log("品赞删除旧ip成功");
-            //添加新白名单
-            let ipzandata = await request({
-                url: "https://service.ipzan.com/whiteList-add?no=" + ipzanNo + "&sign=" + ipzanSign() + "&ip=" + newip,
-                method: "get",
-            });
-            console.log("品赞添加新ip成功");
-            adminNotice += "品赞success\n";
-            //console.log(ipzandata.body);
-        }
-    }
-
-    function ipzanSign() {
-        const CryptoJS = require("crypto-js");
-        //const data = `登录密码:套餐提取密匙:${Date.now() / 1000}`;
-        const data = `${品赞登录密码}:${品赞套餐提取密匙}:${Date.now() / 1000}`;
-        //const key = CryptoJS.enc.Utf8.parse(签名密匙);
-        const key = CryptoJS.enc.Utf8.parse(`${品赞签名密匙}`);
-        const encryptedData = CryptoJS.AES.encrypt(data, key, {
-            mode: CryptoJS.mode.ECB,
-            padding: CryptoJS.pad.Pkcs7,
-        });
-        const sign = encryptedData.ciphertext.toString();
-        return sign;
+    } else {
+        console.log("更换rabbitpro反代：另一个自动更换反代正在运行中");
     }
 };
-function jlwlSign() {
-    const string = "trade_no=" + jltradeNo + "&key=" + jlkey;
-    const md5 = require("md5");
-    const sign = md5(string);
-    return sign;
-}
